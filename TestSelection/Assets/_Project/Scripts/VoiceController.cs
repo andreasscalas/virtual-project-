8f25659e0c6e5780acf3a5a2956cb73890989fc8
YuanjuZHU <eliteyjz@gmail.com>
2020-06-09 14:31:52 +0200

    Move "Voice Control" into Script folder.

---
+++ TestSelection/Assets/_Project/Scripts/VoiceController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class VoiceController : MonoBehaviour
+{
+    private KeywordRecognizer keywordRecognizer;
+    private Dictionary<string, Action> actions= new Dictionary<string, Action>();
+    public TreatSelectionManager treatslectionManager;
+    //public MeshCreateControlPoints meshCreateControlPoints;
+
+    void Start()
+    {
+        actions.Add("translation", treatslectionManager.Translation);
+        actions.Add("rotation", treatslectionManager.Rotation);
+        actions.Add("scale", treatslectionManager.Scale);
+        //actions.Add("change scale", treatslectionManager.ChangeScale);
+        
+
+        keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
+        keywordRecognizer.Start();
+    }
+
+    private void RecognizedSpeech(PhraseRecognizedEventArgs speech)
+    {
+        Debug.Log(speech.text);
+        actions[speech.text].Invoke();
+    }
+
+}
