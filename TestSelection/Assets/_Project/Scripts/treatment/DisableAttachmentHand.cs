71fa3047e2fdd8fd10b081194c99035c10e6c683
YuanjuZHU <eliteyjz@gmail.com>
2020-06-19 16:37:31 +0200

    Hide attachement hands when leap hands are not detected,

---
+++ TestSelection/Assets/_Project/Scripts/treatment/DisableAttachmentHand.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Leap;
+using Leap.Unity;
+using UnityEngine;
+
+public class DisableAttachmentHand : MonoBehaviour
+{
+    public HandModelBase leftHandModel;
+    private GameObject attachmentHands;
+    void Start()
+    {
+        attachmentHands = GameObject.Find("Attachment Hands");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (leftHandModel.IsTracked)
+        {
+            attachmentHands.SetActive(true);
+            Debug.Log("left hand tracked");
+        }
+
+        if (!leftHandModel.IsTracked)
+        {
+            attachmentHands.SetActive(false);
+            //Debug.Log("left hand is not tracked");
+
+        }
+
+    }
+}
+
