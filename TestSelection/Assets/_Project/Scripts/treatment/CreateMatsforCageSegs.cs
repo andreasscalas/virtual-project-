a4ea7b02104e4e26d877c90c706324cc74782eaa
YuanjuZHU <eliteyjz@gmail.com>
2020-06-29 00:04:33 +0200

    A test for creating materials by having the colors

---
+++ TestSelection/Assets/_Project/Scripts/treatment/CreateMatsforCageSegs.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+public class CreateMatsforCageSegs : MonoBehaviour
+{
+    // Start is called before the first frame update
+    static public ReadJson readJson;
+
+    void Start()
+    {
+        readJson = GameObject.Find("Selection Manager").GetComponent<ReadJson>();
+        CreateMaterial();
+    }
+
+    static void CreateMaterial()
+    {
+        // Create a simple material asset
+        
+        for (int i = 0; i < readJson.AllSegColors.Count; i++)
+        {
+            var newMaterial = new Material(Shader.Find("Diffuse"));
+            AssetDatabase.CreateAsset(newMaterial, "Assets/Resources/" + "MaterialSeg"+i + ".mat");
+            newMaterial.color =new Color(readJson.AllSegColors[i][0],readJson.AllSegColors[i][1],readJson.AllSegColors[i][2])/255;
+        }
+        
+        
+    }
+}
