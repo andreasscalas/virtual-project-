8cfd6d77227dbd755c6310482f7163acc76fabeb
YuanjuZHU <eliteyjz@gmail.com>
2020-07-02 10:04:46 +0200

    Add material attributs to the class for the facility of assgin materials

--- TestSelection/Assets/_Project/Scripts/treatment/ControlPointsData.cs
+++ TestSelection/Assets/_Project/Scripts/treatment/ControlPointsData.cs
@@ -9,22 +9,27 @@ namespace Assets._Project.Scripts.treatment
 {
     public class ControlPointsData
     {
-        //public GameObject go;
+        public GameObject go;
         public List<string> goTags=new List<string>();
         public int goIndex;
         public List<Color> goColor = new List<Color>();
+        public Material defautMaterial;
+        public Material outlineMaterial;
 
         public ControlPointsData()
         {
 
         }
 
-        public ControlPointsData(/*GameObject go,*/ List<string> goTags, List<Color> goColor,int goIndex)
+        public ControlPointsData(GameObject go, List<string> goTags, List<Color> goColor,int goIndex, Material defautMaterial, Material outlineMaterial)
         {
-            //this.go = go;
+            this.go = go;
             this.goTags = goTags;
             this.goColor = goColor;
             this.goIndex = goIndex;
+            this.defautMaterial = defautMaterial;
+            this.outlineMaterial = outlineMaterial;
+
 
         }
     }
