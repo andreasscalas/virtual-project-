197f9ce27ea60fcfbea6e3a6a636203e65a68005
YuanjuZHU <eliteyjz@gmail.com>
2020-07-02 10:06:51 +0200

    Test for creating material.

--- TestSelection/Assets/_Project/Scripts/treatment/CreateMatsforCageSegs.cs
+++ TestSelection/Assets/_Project/Scripts/treatment/CreateMatsforCageSegs.cs
@@ -7,23 +7,26 @@ using UnityEngine;
 public class CreateMatsforCageSegs : MonoBehaviour
 {
     // Start is called before the first frame update
-    static public ReadJson readJson;
+    //static public ReadJson readJson;
+    private List<Color> AllSegColors;
 
     void Start()
     {
-        readJson = GameObject.Find("Selection Manager").GetComponent<ReadJson>();
+         AllSegColors =new List <Color> {Color.red, Color.green, Color.blue};
+        //readJson = GameObject.Find("Selection Manager").GetComponent<ReadJson>();
         CreateMaterial();
     }
 
-    static void CreateMaterial()
+    void CreateMaterial()
     {
         // Create a simple material asset
         
-        for (int i = 0; i < readJson.AllSegColors.Count; i++)
+        for (int i = 0; i < AllSegColors.Count; i++)
         {
-            var newMaterial = new Material(Shader.Find("Diffuse"));
-            AssetDatabase.CreateAsset(newMaterial, "Assets/Resources/" + "MaterialSeg"+i + ".mat");
-            newMaterial.color =new Color(readJson.AllSegColors[i][0],readJson.AllSegColors[i][1],readJson.AllSegColors[i][2])/255;
+            var newMaterial = new Material(Shader.Find("Outlined/Silhouetted Diffuse"));
+            AssetDatabase.CreateAsset(newMaterial, "Assets/" + "Material Test"+i + ".mat");
+            newMaterial.SetColor("_Color", AllSegColors[i] );
+            newMaterial.SetColor("_OutlineColor", AllSegColors[2-i]);
         }
         
         
