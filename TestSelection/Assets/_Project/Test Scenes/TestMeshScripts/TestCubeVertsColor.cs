e2d56ced5144866db76f17ca0a38b90ac07c4572
YuanjuZHU <eliteyjz@gmail.com>
2020-07-03 17:31:35 +0200

    Some tests

--- TestSelection/Assets/_Project/Test Scenes/TestMeshScripts/TestCubeVertsColor.cs
+++ TestSelection/Assets/_Project/Test Scenes/TestMeshScripts/TestCubeVertsColor.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class TestCubeVertsColor : MonoBehaviour
 {
     // Update is called once per frame
-    void Update()
+    void Start()
     {
         Mesh mesh = GetComponent<MeshFilter>().mesh;
         Vector3[] vertices = mesh.vertices;
+        int[] triangles = mesh.triangles;
         Color[] colors = new Color[vertices.Length];
         for (int i = 0; i < vertices.Length; i++)
         {
             colors[i] = Color.blue;
         }
+        colors[10] = Color.red; colors[7] = Color.red; colors[11] = Color.red;
+        colors[10] = Color.yellow; colors[6] = Color.yellow; colors[7] = Color.yellow;
         mesh.colors = colors;
         mesh.RecalculateNormals();
+        for (int i = 0; i < 12; i++)
+        {
+            Debug.Log("triangles " + " " + i + "    vertex " + triangles[3*i]+"     vertex position: "+ vertices[triangles[3 * i]]);
+            Debug.Log("triangles " + " " + i + "    vertex " + triangles[3*i+1]+"     vertex position: "+ vertices[triangles[3 * i+1]]);
+            Debug.Log("triangles " + " " + i + "    vertex " + triangles[3*i+2]+"     vertex position: "+ vertices[triangles[3 * i+2]]);
+        }
     }
 }
--- TestSelection/Assets/_Project/Test Scenes/TestMeshScripts/TestMesh.cs
+++ TestSelection/Assets/_Project/Test Scenes/TestMeshScripts/TestMesh.cs
@@ -4,30 +4,50 @@ using UnityEngine;
 
 public class TestMesh : MonoBehaviour
 {
+    private GameObject sphere;
+    
+
+    private Mesh mesh;
+    private Vector3[] vertices = new Vector3[6];
+    int[] triangles = new int[6];
+    Color[] colors = new Color[6];
+
     // Start is called before the first frame update
     void Start()
     {
         gameObject.AddComponent<MeshFilter>();
         gameObject.AddComponent<MeshRenderer>();
         gameObject.transform.position = new Vector3(0, 0, 0);
-        var mesh = gameObject.GetComponent<MeshFilter>().mesh;
-        Vector3[] vertices = new Vector3[6];
+        mesh = gameObject.GetComponent<MeshFilter>().mesh;
+        //Vector3[] vertices = new Vector3[6];
         vertices[0] = new Vector3(0, 0, 0);
         vertices[1] = new Vector3(0, 1, 0);
-        vertices[2] = new Vector3(1, 0, 0);
+        vertices[2] = new Vector3(1, 1, 0);
         vertices[3] = new Vector3(1, 0, 0);
-        vertices[4] = new Vector3(0, 1, 0);
-        vertices[5] = new Vector3(1, 0, 1);
+        //vertices[4] = new Vector3(0, 0, 1);
+        //vertices[5] = new Vector3(0, 1, 1);
+        //vertices[6] = new Vector3(1, 1, 1);
+        //vertices[7] = new Vector3(1, 0, 1);
+        sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = new Vector3(0.2f, 0.2f, 0.2f);
+        vertices[0] = sphere.transform.position;
 
-        int[] triangles=new int[6];
+        //int[] triangles=new int[6];
         triangles[0] = 0;
         triangles[1] = 1;
         triangles[2] = 2;
-        triangles[3] = 3;
-        triangles[4] = 4;
-        triangles[5] = 5;
+        triangles[3] = 2;
+        triangles[4] = 3;
+        triangles[5] = 0;
+        //triangles[6] = 5;
+        //triangles[7] = 5;
+        //triangles[8] = 5;
+        //triangles[9] = 5;
+        //triangles[10] = 5;
+        //triangles[11] = 5;
 
-        Color[] colors = new Color[vertices.Length];
+
+        //Color[] colors = new Color[vertices.Length];
 
         colors[0] = Color.green; 
         colors[1] = Color.green; 
@@ -52,4 +72,13 @@ public class TestMesh : MonoBehaviour
     }
 
     // Update is called once per frame
+
+    void Update()
+    {
+        vertices[0] = sphere.transform.position;
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.colors = colors;
+        mesh.RecalculateNormals();
+    }
 }
