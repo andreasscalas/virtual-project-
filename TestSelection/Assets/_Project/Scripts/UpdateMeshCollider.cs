1cdcc2422f5f98a184f972095ad3d644aae83466
YuanjuZHU <eliteyjz@gmail.com>
2020-07-03 17:26:05 +0200

    Update the mesh collider at real time.

---
+++ TestSelection/Assets/_Project/Scripts/UpdateMeshCollider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateMeshCollider : MonoBehaviour
+{
+
+    Mesh myMesh;
+    private GameObject objCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        objCollider = GameObject.Find("hand");
+        myMesh = objCollider.GetComponentInChildren<MeshFilter>().mesh;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        myMesh.RecalculateBounds();
+
+        MeshCollider myMC = objCollider.GetComponent<MeshCollider>();
+        myMC.sharedMesh = null;
+        myMC.sharedMesh = myMesh;
+    }
+}
