28951fd97f8d83180db9d94de6536c28dc983502
YuanjuZHU <eliteyjz@gmail.com>
2020-07-13 10:07:35 +0200

    A class for the tree node, so that we can store data in a tree.

---
+++ TestSelection/Assets/_Project/Scripts/treatment/TreeNode.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets._Project.Scripts.treatment;
+
+class TreeNode : IEnumerable<TreeNode>
+{
+    private readonly Dictionary<ModelData, TreeNode> _children =
+        new Dictionary<ModelData, TreeNode>();
+
+    public readonly ModelData ID;
+    public TreeNode Parent { get; private set; }
+
+    public TreeNode(ModelData id)
+    {
+        this.ID = id;
+    }
+
+    public TreeNode GetChild(ModelData id)
+    {
+        return this._children[id];
+    }
+
+    public void Add(TreeNode item)
+    {
+        if (item.Parent != null)
+        {
+            item.Parent._children.Remove(item.ID);
+        }
+
+        item.Parent = this;
+        this._children.Add(item.ID, item);
+    }
+
+    public IEnumerator<TreeNode> GetEnumerator()
+    {
+        return this._children.Values.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+
+    public int Count
+    {
+        get { return this._children.Count; }
+    }
+
+
+}
+
+
+
+
