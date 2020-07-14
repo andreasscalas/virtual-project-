88c328f7a69019ba6f13a05187ba983031fb1ce2
YuanjuZHU <eliteyjz@gmail.com>
2020-07-14 11:05:41 +0200

    Add levelsChange bool variable to detect levels change; add levels to judg which we are in level .

--- TestSelection/Assets/_Project/Scripts/Functionality.cs
+++ TestSelection/Assets/_Project/Scripts/Functionality.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,19 +8,32 @@ public class Functionality : MonoBehaviour
 {
     public GameObject hand_cage;
     private Image image;
+
+    public ReadJson readJson;
+    public List<bool> levels=new List<bool>();
+    public bool levelsChange=new bool();
+
     // Start is called before the first frame update
     void Start()
     {
+        levelsChange = false;
         hand_cage = GameObject.Find("hand_cage");
         //transform.horse_cage().Where(x => x.name.Contains("Patrol"));
         image = GameObject.Find("Hide/Display Cage").GetComponent<Image>();
         image.color = new Color(0f, 0.6f, 1f, 1f);
+
+        readJson = GameObject.Find("Selection Manager").GetComponent<ReadJson>();
+        for (int i = 0; i < readJson.modelLevel.Count; i++)
+        {
+            levels.Add(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        levelsChange = false;
+        Debug.Log("levelsChange update "+ levelsChange);
     }
 
     public void HideDisplayCage()
@@ -36,4 +50,29 @@ public class Functionality : MonoBehaviour
             image.color = new Color(0f, 0.6f, 1f, 1f);
         }
     }
+
+
+    void OnGUI()
+    {
+        //find out the levels, so that we can decide how many bouttons we need.
+        for (int i = 0; i < readJson.modelLevel.Count; i++)
+        {
+            var myButton= GUI.Button(new Rect(10, 30*i, 50, 30), "level " + i);
+            if (myButton)
+            {
+                levelsChange = true;
+                for (int j = 0; j < levels.Count; j++)
+                {
+                    levels[j]=false;
+                }
+                levels[i] = true;
+                Debug.Log(String.Format("button{0} is pressed: ", i));
+                for (int j = 0; j < levels.Count; j++)
+                {
+                    Debug.Log(String.Format("levels {0}:{1} ", j, levels[j]));
+                }
+            }
+        }
+        Debug.Log("levelsChange ongui " + levelsChange);
+    }   
 }
