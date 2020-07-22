0add59cb6ba20e9b03513ec1d7b0851823fe2047
YuanjuZHU <eliteyjz@gmail.com>
2020-07-22 10:10:51 +0200

    Add properties and methodes to tree tree node class.

--- TestSelection/Assets/_Project/Scripts/treatment/TreeNode.cs
+++ TestSelection/Assets/_Project/Scripts/treatment/TreeNode.cs
@@ -1,21 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Assets._Project.Scripts.treatment;
+using UnityEngine;
 
-class TreeNode : IEnumerable<TreeNode>
+public class TreeNode : IEnumerable<TreeNode>
 {
-    private readonly Dictionary<ModelData, TreeNode> _children =
-        new Dictionary<ModelData, TreeNode>();
-
-    public readonly ModelData ID;
+    private readonly Dictionary<int, TreeNode> _children =
+        new Dictionary<int, TreeNode>();
+    public readonly int ID;
     public TreeNode Parent { get; private set; }
+    private GameObject go;
+    private ModelData data;
+    private int Level;
+    public bool visited = false;
 
-    public TreeNode(ModelData id)
+    public TreeNode(ModelData data)
     {
-        this.ID = id;
+        this.data = data;
+        this.ID = data.id;
+        this.Level = 0;
+        this.go = new GameObject();
+        this.go.name = data.tag;
     }
 
-    public TreeNode GetChild(ModelData id)
+    public GameObject GetGameobject()
+    {
+        return go;
+    }
+
+    public ModelData GetData()
+    {
+        return this.data;
+    }
+
+    public int GetLevel()
+    {
+        return Level;
+    }
+
+    public void SetLevel(int Level)
+    {
+        this.Level = Level;
+    }
+
+    public List<TreeNode> GetChildren()
+    {
+        var childrenList = new List<TreeNode>();
+        foreach (var child in _children.Values)
+        {
+            childrenList.Add(child);
+        }
+
+        //var childrenList =  _children.Values.SelectMany(x => x).ToList();
+        return childrenList;
+        
+    }
+
+
+    public TreeNode GetChild(int id)
     {
         return this._children[id];
     }
@@ -27,8 +70,10 @@ class TreeNode : IEnumerable<TreeNode>
             item.Parent._children.Remove(item.ID);
         }
 
-        item.Parent = this;
         this._children.Add(item.ID, item);
+        item.Parent = this;
+        item.SetLevel(Level + 1);
+        item.GetGameobject().transform.parent = item.Parent.GetGameobject().transform;
     }
 
     public IEnumerator<TreeNode> GetEnumerator()
@@ -45,6 +90,28 @@ class TreeNode : IEnumerable<TreeNode>
     {
         get { return this._children.Count; }
     }
+
+    public TreeNode GetDescendent(int id)
+    {
+        if (this.ID==id)
+        {
+            return this;
+        }
+
+        var children = this.GetChildren();
+
+        for (int i = 0; i < children.Count; i++)
+        {
+            var result = children[i].GetDescendent(id);
+            if (result!=null)
+            {
+                return result;
+            }
+        }
+
+        return null;
+    }
+
 
 
 }
