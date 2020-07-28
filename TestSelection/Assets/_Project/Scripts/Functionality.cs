8f0d651a2431bfff0913403a65b13cc6e4c564b4
YuanjuZHU <eliteyjz@gmail.com>
2020-07-28 12:08:35 +0200

    rename "hand_cage" to "model", display/hide control points together with cage

--- TestSelection/Assets/_Project/Scripts/Functionality.cs
+++ TestSelection/Assets/_Project/Scripts/Functionality.cs
@@ -6,21 +6,23 @@ using UnityEngine.UI;
 
 public class Functionality : MonoBehaviour
 {
-    public GameObject hand_cage;
+    public GameObject model;
     private Image image;
 
-    public ReadJson readJson;
+    private ReadJson readJson;
     //public List<bool> levels=new List<bool>();
-    public List<bool> levels=new List<bool>();
-    public bool levelsChange=new bool();
+    [HideInInspector] public List<bool> levels=new List<bool>();
+    [HideInInspector] public bool levelsChange=new bool();
+    private List<GameObject> controlPoints = new List<GameObject>();
+
 
     // Start is called before the first frame update
     void Start()
     {
         levelsChange = true;
-        hand_cage = GameObject.Find("hand_cage");
+        model = GameObject.Find("hand_cage");
         //transform.horse_cage().Where(x => x.name.Contains("Patrol"));
-        image = GameObject.Find("Hide/Display Cage").GetComponent<Image>();
+        image = GameObject.Find("Hide/show Cage&CPs").GetComponent<Image>();
         image.color = new Color(0f, 0.6f, 1f, 1f);
 
         readJson = GameObject.Find("Selection Manager").GetComponent<ReadJson>();
@@ -29,6 +31,18 @@ public class Functionality : MonoBehaviour
             levels.Add(false);
         }
         levels[0] = true;
+
+
+        GameObject[] objs = (GameObject[])FindObjectsOfType(typeof(GameObject));
+        
+        for (int i = 0; i < objs.Length; i++)
+        {
+            if (objs[i].name.Contains("Control Point"))
+            {
+                controlPoints.Add(objs[i]);
+            }
+        }
+
     }
 
     // Update is called once per frame
@@ -40,38 +54,46 @@ public class Functionality : MonoBehaviour
     public void HideDisplayCage()
     {
         
-        if (hand_cage.activeSelf)
+        if (model.activeSelf)
         {
-            hand_cage.SetActive(false);
+            model.SetActive(false);
+            foreach (var VARIABLE in controlPoints)
+            {
+                VARIABLE.SetActive(false);
+            }
             image.color = new Color(0f, 1f, 0.6f, 1f);
         }
         else
         {
-            hand_cage.SetActive(true);
+            model.SetActive(true);
+            foreach (var VARIABLE in controlPoints)
+            {
+                VARIABLE.SetActive(true);
+            }
             image.color = new Color(0f, 0.6f, 1f, 1f);
         }
     }
 
 
-    void OnGUI()
-    {
-        //find out the levels, so that we can decide how many bouttons we need.
+    //void OnGUI()
+    //{
+    //    //find out the levels, so that we can decide how many bouttons we need.
 
 
 
-        for (int i = 0; i <= readJson.levelMax; i++)
-        {
-            var myButton = GUI.Button(new Rect(10, 30 * i, 50, 30), "level " + i);
-            if (myButton)
-            {
-                levelsChange = true;
-                for (int j = 0; j < levels.Count; j++)
-                {
-                    levels[j] = false;
-                }
-                levels[i] = true;
-                Debug.Log(String.Format("button{0} is pressed: ", i));
-            }
-        }
-    }   
+    //    for (int i = 0; i <= readJson.levelMax; i++)
+    //    {
+    //        var myButton = GUI.Button(new Rect(10, 30 * i, 50, 30), "level " + i);
+    //        if (myButton)
+    //        {
+    //            levelsChange = true;
+    //            for (int j = 0; j < levels.Count; j++)
+    //            {
+    //                levels[j] = false;
+    //            }
+    //            levels[i] = true;
+    //            Debug.Log(String.Format("button{0} is pressed: ", i));
+    //        }
+    //    }
+    //}   
 }
