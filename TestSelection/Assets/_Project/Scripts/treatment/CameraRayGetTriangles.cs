4ca9226f48fdca732dff28c860fc3f768d6e8f05
YuanjuZHU <eliteyjz@gmail.com>
2020-07-28 12:13:38 +0200

    outline/ select/ delete a set of control points.

--- TestSelection/Assets/_Project/Scripts/treatment/CameraRayGetTriangles.cs
+++ TestSelection/Assets/_Project/Scripts/treatment/CameraRayGetTriangles.cs
@@ -8,22 +8,35 @@ public class CameraRayGetTriangles : MonoBehaviour
 {
     Camera cam;
     private ReadJson readJson;
-    public Functionality functionality;
-    public MeshCreateControlPoints meshCreateControlPoints;
-    public List<ControlPointsData> myList = new List<ControlPointsData>();
+    private Functionality functionality;
+    private MeshCreateControlPoints meshCreateControlPoints;
+    private TreatSelectionManager treatSelectionManager;
+    private VoiceController voiceController;
+    [HideInInspector] public List<ControlPointsData> myList = new List<ControlPointsData>();
     private bool hitMeshCollider;
     private int flagSegment;
-    private Transform _selection;
+    private List<int> intersectSegmentIndexes = new List<int>();
+    private List<int> intersectSegmentIndexesOld = new List<int>();
+    [SerializeField] private Material defaultMaterial;
+    [SerializeField] private Material OutlineMaterial2;
+    [SerializeField] private Material SelectedMaterial;
+    private List<ControlPointsData> controlPointsBySegments = new List<ControlPointsData>();
+    private bool match;
+
     void Start()
     {
         readJson = GameObject.Find("Selection Manager").GetComponent<ReadJson>();
         functionality = GameObject.Find("Selection Manager").GetComponent<Functionality>();
         meshCreateControlPoints = GameObject.Find("Selection Manager").GetComponent<MeshCreateControlPoints>();
+        treatSelectionManager = GameObject.Find("Selection Manager").GetComponent<TreatSelectionManager>();
+        voiceController = GameObject.Find("Selection Manager").GetComponent<VoiceController>();
 
         cam = GetComponent<Camera>();
         hitMeshCollider = false;
         flagSegment = -1;
 
+        //controlPointsBySegments
+        //meshHand = GameObject.Find("hand").GetComponent<MeshCollider>();
     }
 
     void Update()
@@ -31,86 +44,184 @@ public class CameraRayGetTriangles : MonoBehaviour
         RaycastHit hit;
         Ray ray = new Ray(Camera.main.transform.position, 1000 * Camera.main.transform.forward);
 
-       
-        if (!Physics.Raycast(ray, out hit))
+        hitMeshCollider = Physics.Raycast(ray, out hit);
+
+        // if the mesh is not hit
+        if (!hitMeshCollider /* && var bool ==true*/)
         {
-            
-            hitMeshCollider = false;
-            Debug.Log("hitMeshCollider:" + hitMeshCollider);
-            return ;
-        }
-        else
-        {
-            Debug.Log("hit.transform.gameObject.name:" + hit.transform.gameObject.name);
-            hitMeshCollider = true;
-            Debug.Log("hitMeshCollider:" + hitMeshCollider);
-        }
-
-        MeshCollider meshCollider = hit.collider as MeshCollider;
-        if (meshCollider == null || meshCollider.sharedMesh == null)
-        {
-            hitMeshCollider = false;
-            return;
-        }
-        Mesh mesh = meshCollider.sharedMesh;
-        Vector3[] vertices = mesh.vertices;
-        int[] triangles = mesh.triangles;
-
-        Vector3 p0 = vertices[triangles[hit.triangleIndex * 3 + 0]];
-        Vector3 p1 = vertices[triangles[hit.triangleIndex * 3 + 1]];
-        Vector3 p2 = vertices[triangles[hit.triangleIndex * 3 + 2]];
-
-
-        //for (int k = 0; k < functionality.levels.Count; k++)
-        //{
-            //if (functionality.levels[k] == true)
-            //{
-
-                //Debug.Log("meshCollider.name "+ meshCollider.name);
-                //Debug.Log("hit.collider.name " + hit.collider.name);
-                //Debug.Log("hit.transform " + hit.transform);
-                Debug.Log("hitMeshCollider " + hitMeshCollider);
-                if (/*flagSegment >= 0 ||*/ !hitMeshCollider /*|| _selection!=null*/)
+            //get all Control Points Data///////////////////
+            for (int i = 0; i < meshCreateControlPoints.cpDataList.Count; i++)
+            {
+                var controlPoints = meshCreateControlPoints.cpDataList[i];
+                //if the control points are selected, reset default material 
+                if (treatSelectionManager.selectionList.Contains(controlPoints.go.transform))
                 {
-                    DefaultControPoints(flagSegment);
-                    Debug.Log("CP has been set default material");
-                    flagSegment = -1;
-                    //_selection = null;
+                    controlPoints.go.GetComponent<MeshRenderer>().material = SelectedMaterial ;
                 }
 
-                else
-                //if (hitMeshCollider)
+                //the not selected CPs
+
+                if (!treatSelectionManager.selectionList.Contains(controlPoints.go.transform))
                 {
-                    for (int i = 0; i < readJson.treeNodeLevelx.Count; i++)
-                    {
-
-                        if ((readJson.treeNodeLevelx[i].GetData().triangles.Contains(hit.triangleIndex)))
-                        {
-                            //Debug.Log("flagSegment:" );
-                            OutlineControPoints(i);
-                            Debug.Log("CP has been set highlight material");
-                            flagSegment = i;
-                            //_selection = hit.transform;
-                        }
-
-                    }
+                    controlPoints.go.GetComponent<MeshRenderer>().material = controlPoints.defautMaterial;
                 }
                 
-            //}
-        //}
+                
+                //A bool var becomes false here/////////////////////
+            }
+
+        }
+        // if the mesh is hit
+        else
+        {
+            for (int k = 0; k < meshCreateControlPoints.cpDataList.Count; k++)
+            {
+                if (!match)
+                {
+                    Debug.Log("not match");
+                    for (int x = 0; x < intersectSegmentIndexesOld.Count; x++)
+                    {
+                        //find out the control points that has te same tags(belong to a same segment) 
+                        var controlPointsOfSegmentsHitOld = meshCreateControlPoints.cpDataList.FindAll(y => y.goTags.Contains(readJson.treeNodeLevelx[intersectSegmentIndexesOld[x]].GetData().tag));
+
+                        if (meshCreateControlPoints.cpDataList[k].goTags.Contains(readJson.treeNodeLevelx[intersectSegmentIndexesOld[x]].GetData().tag))
+                        {
+                            //if the control point has been already selected
+                            if (treatSelectionManager.selectionList.Contains(meshCreateControlPoints.cpDataList[k].go.transform))
+                            {
+
+                                foreach (var VARIABLE in controlPointsOfSegmentsHitOld)
+                                {
+                                    VARIABLE.go.GetComponent<MeshRenderer>().material = SelectedMaterial;
+                                }
+                            }
+                            if (!treatSelectionManager.selectionList.Contains(meshCreateControlPoints.cpDataList[k].go.transform))
+                            {
+                                foreach (var VARIABLE in controlPointsOfSegmentsHitOld)
+                                {
+                                    VARIABLE.go.GetComponent<MeshRenderer>().material = meshCreateControlPoints.cpDataList[k].defautMaterial;
+                                }
+                            }
+
+                        }
+                    }
+                    intersectSegmentIndexesOld.Clear();
+                    for (int i = 0; i < intersectSegmentIndexes.Count; i++)
+                    {
+                        intersectSegmentIndexesOld.Add(intersectSegmentIndexes[i]);
+                    }
+                    
+                }
+
+            }
+
+            //Debug.Log("intersectSegmentIndexesOld " + intersectSegmentIndexesOld.Count);
+            intersectSegmentIndexes.Clear();
+
+            //go through the all segments
+            for (int i = 0; i < readJson.treeNodeLevelx.Count; i++)
+            {
+                //go though all the triangles indexes of a segment
+                for (int j = 0; j < readJson.treeNodeLevelx[i].GetData().triangles.Count; j++)
+                {
+                    //ray hits a segment, save the segment number in a list
+                    if (hit.triangleIndex == readJson.treeNodeLevelx[i].GetData().triangles[j])
+                    {
+                        //intersectSegmentIndexesOld = intersectSegmentIndexes;
+
+                        intersectSegmentIndexes.Add(i);
+
+                        match = CheckMatch(intersectSegmentIndexesOld, intersectSegmentIndexes);
+                        Debug.Log("intersectSegmentIndexes " + intersectSegmentIndexes[0]);
+                        // high light these segments
+                        // go though the control points
 
 
-        //Vector3 p0 = vertices[triangles[hit.triangles * 3 + 0]];
-        //Vector3 p1 = vertices[triangles[hit.triangles * 3 + 1]];
-        //Vector3 p2 = vertices[triangles[hit.triangles * 3 + 2]];
-        Transform hitTransform = hit.collider.transform;
-        p0 = hitTransform.TransformPoint(p0);
-        p1 = hitTransform.TransformPoint(p1);
-        p2 = hitTransform.TransformPoint(p2);
-        Debug.DrawLine(p0, p1);
-        Debug.DrawLine(p1, p2);
-        Debug.DrawLine(p2, p0);
-        //Debug.DrawRay(cam.transform.position, 1000 * Camera.main.transform.forward);
+                        
+
+
+
+
+
+                        //go though all the control points instances
+                        for (int k = 0; k < meshCreateControlPoints.cpDataList.Count; k++)
+                        {
+                            //go through all the segments that are hit
+                            for (int x = 0; x < intersectSegmentIndexes.Count; x++)
+                            {
+                                //find out the control points that has te same tags(belong to a same segment) 
+                                var controlPointsOfSegmentsHit = meshCreateControlPoints.cpDataList.FindAll(y => y.goTags.Contains(readJson.treeNodeLevelx[intersectSegmentIndexes[x]].GetData().tag));
+
+                                if (meshCreateControlPoints.cpDataList[k].goTags.Contains(readJson.treeNodeLevelx[intersectSegmentIndexes[x]].GetData().tag))
+                                {
+                                    //if the control point has been already selected
+                                    if (treatSelectionManager.selectionList.Contains(meshCreateControlPoints.cpDataList[k].go.transform))
+                                    {
+                                        foreach (var VARIABLE in controlPointsOfSegmentsHit)
+                                        {
+                                            VARIABLE.go.GetComponent<MeshRenderer>().material = OutlineMaterial2;
+                                        }
+                                        
+                                    }
+                                    if (!treatSelectionManager.selectionList.Contains(meshCreateControlPoints.cpDataList[k].go.transform))
+                                    {
+                                        foreach (var VARIABLE in controlPointsOfSegmentsHit)
+                                        {
+                                            VARIABLE.go.GetComponent<MeshRenderer>().material = meshCreateControlPoints.cpDataList[k].outlineMaterial;
+                                        }
+                                    }
+
+                                }
+                            }
+
+                        }
+
+                        
+
+                    }
+                    ////ray hits mesh but not a segment on the mesh
+                    //if (intersectSegmentIndexes == null)
+                    //{
+                    //    // a repeating part ///////////////////
+                    //    for (int k = 0; k < meshCreateControlPoints.cpDataList.Count; k++)
+                    //    {
+                    //        var controlPoints = meshCreateControlPoints.cpDataList[k];
+                    //        //if the control points are selected, reset default material 
+                    //        if (treatSelectionManager.selectionList.Contains(controlPoints.go.transform))
+                    //        {
+                    //            controlPoints.go.GetComponent<MeshRenderer>().material = controlPoints.defautMaterial;
+                    //        }
+
+                    //        //the not selected CPs
+                    //        else
+                    //        {
+                    //            controlPoints.go.GetComponent<MeshRenderer>().material = SelectedMaterial;
+                    //        }
+                    //        //A bool var becomes false here/////////////////////
+                    //    }
+                    //}
+
+                }
+            }
+
+            
+        }
+
+
+
+
+    }
+
+    private bool CheckMatch(List<int> l1, List<int> l2)
+    {
+        if (l1.Count != l2.Count)
+            return false;
+        for (int i = 0; i < l1.Count; i++)
+        {
+            if (l1[i] != l2[i])
+                return false;
+        }
+        return true;
     }
 
     private void OutlineControPoints(int flagSegment)
@@ -125,12 +236,15 @@ public class CameraRayGetTriangles : MonoBehaviour
 
             for (int j = 0; j < meshCreateControlPoints.materialGroup1.Count; j++)
             {
-                if (myList[m].goColor[0] == meshCreateControlPoints.materialGroup1[j].color)
+                if (myList[m].goColor[0] == meshCreateControlPoints.materialGroup1[j].color )
                 {
                     controlPointRenderer.material = meshCreateControlPoints.outlineMaterialGroup1[j];
+                    
                 }
+                
             }
         }
+        voiceController.segmentSelect = false;
     }
 
 
@@ -145,12 +259,14 @@ public class CameraRayGetTriangles : MonoBehaviour
 
             for (int j = 0; j < meshCreateControlPoints.materialGroup1.Count; j++)
             {
-                if (myList[m].goColor[0] == meshCreateControlPoints.materialGroup1[j].color)
+                if (myList[m].goColor[0] == meshCreateControlPoints.materialGroup1[j].color )
                 {
-                    controlPointRenderer.material = meshCreateControlPoints.materialGroup1[j];
+                    
                 }
+                
             }
         }
+        
     }
 
 
