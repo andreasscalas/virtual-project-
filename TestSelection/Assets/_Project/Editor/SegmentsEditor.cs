12560746299037e22ad9d87c6712e97dbb8a6182
YuanjuZHU <eliteyjz@gmail.com>
2020-07-28 12:09:16 +0200

    No need for this editor anymore.

--- TestSelection/Assets/_Project/Editor/SegmentsEditor.cs
+++ TestSelection/Assets/_Project/Editor/SegmentsEditor.cs
@@ -12,89 +12,65 @@ public class SegmentEditor : Editor {
 
 		GUILayout.BeginHorizontal();
 
-        if (GUILayout.Button("thumb"))
-        {
-            readJson.trianCageSegmented.Clear();
-            readJson.trianModelSegmented.Clear();
-            readJson.MapSegModel(readJson.thumb);
-            readJson.switchSegment = true;
-        }
+        //if (GUILayout.Button("thumb"))
+        //{
+        //    readJson.trianCageSegmented.Clear();
+        //    readJson.trianModelSegmented.Clear();
+        //    readJson.MapSegModel(readJson.thumb);
+        //    readJson.switchSegment = true;
+        //}
 
-        if (GUILayout.Button("Index finger"))
-        {
-            readJson.trianCageSegmented.Clear();
-            readJson.trianModelSegmented.Clear();
-            readJson.MapSegModel(readJson.indexFinger);
-            readJson.switchSegment = true;
+        //if (GUILayout.Button("Index finger"))
+        //{
+        //    readJson.trianCageSegmented.Clear();
+        //    readJson.trianModelSegmented.Clear();
+        //    readJson.MapSegModel(readJson.indexFinger);
+        //    readJson.switchSegment = true;
             
-        }
+        //}
         
 
-        if (GUILayout.Button("Medium finger"))
-        {
-            readJson.trianCageSegmented.Clear();
-            readJson.trianModelSegmented.Clear();
-            readJson.MapSegModel(readJson.mediumFinger);
-            readJson.switchSegment = true;
-        }
+        //if (GUILayout.Button("Medium finger"))
+        //{
+        //    readJson.trianCageSegmented.Clear();
+        //    readJson.trianModelSegmented.Clear();
+        //    readJson.MapSegModel(readJson.mediumFinger);
+        //    readJson.switchSegment = true;
+        //}
         
-        if (GUILayout.Button("Ring finger"))
-        {
-            readJson.trianCageSegmented.Clear();
-            readJson.trianModelSegmented.Clear();
-            readJson.MapSegModel(readJson.ringFinger);
-            readJson.switchSegment = true;
-        }
-
-        if (GUILayout.Button("Little finger"))
-        {
-            readJson.trianCageSegmented.Clear();
-            readJson.trianModelSegmented.Clear();
-            readJson.MapSegModel(readJson.littleFinger);
-            readJson.switchSegment = true;
-        }
-
-
-        if (GUILayout.Button("palm"))
-        {
-            readJson.trianCageSegmented.Clear();
-            readJson.trianModelSegmented.Clear();
-            readJson.MapSegModel(readJson.palm);
-            readJson.switchSegment = true;
-        }
-
-        if (GUILayout.Button("Wrist"))
-        {
-            readJson.trianCageSegmented.Clear();
-            readJson.trianModelSegmented.Clear();
-            readJson.MapSegModel(readJson.wrist);
-            readJson.switchSegment = true;
-        }
-
-        //if (GUILayout.Button("Medium Finger"))
+        //if (GUILayout.Button("Ring finger"))
         //{
-        //    readJson.Reset();
+        //    readJson.trianCageSegmented.Clear();
+        //    readJson.trianModelSegmented.Clear();
+        //    readJson.MapSegModel(readJson.ringFinger);
+        //    readJson.switchSegment = true;
         //}
 
-        //if (GUILayout.Button("Index Finger"))
+        //if (GUILayout.Button("Little finger"))
         //{
-        //    readJson.Reset();
+        //    readJson.trianCageSegmented.Clear();
+        //    readJson.trianModelSegmented.Clear();
+        //    readJson.MapSegModel(readJson.littleFinger);
+        //    readJson.switchSegment = true;
         //}
 
-        //if (GUILayout.Button("Thumb"))
-        //{
-        //    readJson.Reset();
-        //}
-
-        //if (GUILayout.Button("wrist"))
-        //{
-        //    readJson.Reset();
-        //}
 
         //if (GUILayout.Button("palm"))
         //{
-        //    readJson.Reset();
+        //    readJson.trianCageSegmented.Clear();
+        //    readJson.trianModelSegmented.Clear();
+        //    readJson.MapSegModel(readJson.palm);
+        //    readJson.switchSegment = true;
         //}
+
+        //if (GUILayout.Button("Wrist"))
+        //{
+        //    readJson.trianCageSegmented.Clear();
+        //    readJson.trianModelSegmented.Clear();
+        //    readJson.MapSegModel(readJson.wrist);
+        //    readJson.switchSegment = true;
+        //}
+
 
         GUILayout.EndHorizontal();
 	}
